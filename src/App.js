72b6c7e7bd2de39a2e26e737ffbc0aebdb303c48
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-25 13:14:39 +0600

    selected product show with modal

--- src/App.js
+++ src/App.js
@@ -2,11 +2,37 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Header/Header';
+import Modal from 'react-modal';
+
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
+
 
 function App() {
   const [guns,setGuns] = useState([])
   const [cart,setCart] = useState([])
+  const [modalIsOpen, setIsOpen] = useState(false);
+
   console.log(cart)
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   const handleCart = (gun) => {
     const newCart = [...cart, gun]
@@ -22,16 +48,27 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <div>
-        {
-          cart.map(item => <h3 key = { item.id}> {item.id} {item.name}</h3>)
-        }
+      <div className="modal-btn-container">
+        <button className="modal-btn" onClick={openModal}>Open Your Cart list</button>
       </div>
       <div className = "gun-container">
         {
           guns.map(gun => <Cart key={gun.id} gunData = {gun} handleCart={handleCart}/>)
         }
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>Clode</button>
+        <div>
+        {
+          cart.map(item => <h3 key = { item.id}> {item.id} {item.name}</h3>)
+        }
+      </div>
+      </Modal>
     </div>
   );
 }
