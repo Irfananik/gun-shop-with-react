65f188243ecffbc69596a5098ff928707461aa0e
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-03-25 10:08:42 +0600

    api load done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
+  const [guns,setGuns] = useState([])
+
+  useEffect(() => {
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => setGuns(data))
+  },[])
+
   return (
     <div className="App">
-      
+      <Header/>
+      {
+        guns.map(gun => <h3>{gun.name}</h3>)
+      }
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className="header-container">
+            <div className="header-logo">
+                <h2>Gun cai.com</h2>
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
